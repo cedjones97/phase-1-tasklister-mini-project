3922bf378de0af187cb96832664885390bc92a01
cedric <cedricj765@gmail.com>
2023-04-18 11:03:28 -0500

    completed lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+   document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault()
+    
+     buildToDo(updateId.value)
+   
+  })
+
+
 });
+
+const updateId = document.querySelector('#new-task-description')
+
+
+function buildToDo(todo){
+   let p = document.createElement('p')
+   p.textContent = `${todo} `
+   document.querySelector('#list').appendChild(p)
+   let btn = document.createElement('button')
+   btn.addEventListener('click', handleDelete)
+   btn.textContent = ' x'
+   p.appendChild(btn)
+
+}
+
+
+function handleDelete(e) {
+   e.target.parentNode.remove()
+}
